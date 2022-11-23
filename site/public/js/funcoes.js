4be5e0123b787080b634ed94feaee72d5e07d80b
CarlosVianaRogerio <carlos.rogerio@sptech.school>
2022-11-23 11:56:12 -0300

    cadastro funcionando bala

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -13,7 +13,7 @@ function validarSessao() {
 
         // finalizarAguardar();
     } else {
-        window.location = "../login.html";
+        // window.location = "../login.html";
     }
 }
 
@@ -21,23 +21,24 @@ function limparSessao() {
     // aguardar();
     sessionStorage.clear();
     // finalizarAguardar();
-    window.location = "../login.html";
+    // window.location = "../login.html";
+    
 }
 
 // carregamento (loading)
 function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
+    // var divAguardar = document.getElementById("div_aguardar");
+    // divAguardar.style.display = "flex";      
 }
 
 function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+    // var divAguardar = document.getElementById("div_aguardar");
+    // divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.innerHTML = texto;
-    }
+    // var divErrosLogin = document.getElementById("div_erros_login");
+    // if (texto) {
+    //     divErrosLogin.innerHTML = texto;
+    // }
 }
 
 
--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "bodybuild",
-    user: "root",
-    password: "dededede",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
