8aa66211b13996a327dd20d2f4e341c805f4746f
CarlosVianaRogerio <carlos.rogerio@sptech.school>
2022-11-23 23:07:37 -0300

    script banco

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -5,6 +5,35 @@ CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
-);
-select * from usuario;	
+	senha VARCHAR(50),
+    fkhabilidade int,
+    foreign key (fkhabilidade) references habilidade (idhabilidade));
+
+select * from usuario;
+
+create table quiz (
+	idquiz int primary key auto_increment,
+    pratica int,
+    autoestima int,
+    sono int,
+    costas int,
+    estresse int,
+    concentracao int,
+    fkusuario int, 
+    foreign key (fkusuario) references usuario (id));
+    
+create table habilidade (
+	idhabilidade int primary key auto_increment,
+    saude int,
+    autoestima int,
+    disposicao int);
+    
+    
+    alter table habilidade drop column disposicao;
+    alter table habilidade add column saude int;
+    alter table habilidade add column autoestima int;
+    alter table habilidade add column disposicao int;
+    select * from habilidade;
+    
+    
+    
